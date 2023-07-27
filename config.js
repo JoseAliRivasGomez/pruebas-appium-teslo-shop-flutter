7d4bef481ccc4caf918fc6fef4c6b73d7438c830
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2023-07-27 14:31:26 -0600

    done

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const capabilities = {
     platformName: 'Android',
     'appium:deviceName': 'emulator-5554',
-    'appium:app': 'C:/Users/Usuario/Desktop/Desktop/Cursos/19 Flutter 2023/teslo-shop-app/build/app/outputs/flutter-apk/app-debug.apk',
+    //'appium:app': 'C:/Users/Usuario/Desktop/Desktop/Cursos/19 Flutter 2023/teslo-shop-app/build/app/outputs/flutter-apk/app-debug.apk',
+    'appium:app': 'C:/Users/Usuario/Desktop/Desktop/UTN/Calidad del Software/Proyecto/teslo-shop-qa-appium/teslo-shop-app-qa/build/app/outputs/flutter-apk/app-debug.apk',
     'appium:automationName': 'Flutter',
     //'appium:retryBackoffTime': 500
 }
